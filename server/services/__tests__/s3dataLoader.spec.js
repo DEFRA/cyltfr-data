6d1d54d38ecb89df5eeb757957eb69677c0f93e7
michael steinacher <steinacherm@hotmail.co.uk>
2025-01-27 10:43:40 +0000

    Added second test but currently unable to progress it though loadFeature function.

--- server/services/__tests__/s3dataLoader.spec.js
+++ server/services/__tests__/s3dataLoader.spec.js
@@ -5,10 +5,6 @@ import { getCache } from '../serverCache.js'
 
 jest.mock('../../config.js')
 jest.mock('../serverCache.js')
-jest.mock('../s3dataLoader.js', () => ({
-  ...jest.requireActual('../s3dataLoader.js'),
-  transformToString: jest.fn()
-}))
 const s3Mock = mockClient(S3Client)
 
 describe('s3dataLoader', () => {
@@ -28,11 +24,29 @@ describe('s3dataLoader', () => {
       if (key === 'data') return mockCachedData
     })
 
-    const { transformToString } = require('../s3dataLoader.js')
-    transformToString.mockResolvedValue(JSON.stringify(mockCachedData))
-
     const data = await s3DataLoader()
 
     expect(data).toEqual(mockCachedData)
+  })
+
+  it.only('should save and return the data if manifest file has been modified', async () => {
+    const mockLastModified = '2023-10-02T00:00:00.000Z' // New last modified date
+    const mockCachedModified = '2023-10-01T00:00:00.000Z' // Cached last modified date
+    const mockCachedData = { key: 'oldValue' }
+    const mockNewData = { key: 'newValue' }
+    s3Mock.on(HeadObjectCommand).resolves({ LastModified: new Date(mockLastModified) })
+
+    s3Mock.on(GetObjectCommand).resolves({ Body: { transformToString: jest.fn(() => {
+      return JSON.stringify(mockNewData)
+    }) } })
+
+    getCache.mockImplementation((key) => {
+      if (key === 'lastModified') return mockCachedModified
+      if (key === 'data') return mockCachedData
+    })
+
+    const data = await s3DataLoader()
+
+    expect(data).toEqual(mockNewData)
   })
 })
